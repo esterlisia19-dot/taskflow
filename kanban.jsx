64e480aad9a09880627fd585ba1edb261582e046
Ester <esterlisia19@gmail.com>
2026-08-21 11:52:07 -0300

    opa

--- kanban.jsx
+++ kanban.jsx
@@ -1,4 +1,4 @@
-// teste ok!!!
+// teste ok!!! aaaaaa
 import Header from '../componentes/Header';
 import ListaTarefas from '../componentes/ListaTarefas';
 import { useState, useEffect } from 'react';
